95436f9a6a94174ed679a5831acd5fe571b856a4
Annemarie-Karahure <akarahure89@gmail.com>
2024-10-19 20:13:52 +1100

    Update commit

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,15 @@
+<template>
+ <header>
+<nav class="container flex flex-col sm:flex-row item-center gap-4 text-white py">
+
+
+</nav>
+
+
+ </header>
+</template>
+
+<script setup>
+
+</script>
+ 
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html","./src/**/*.{vue,js,ts,jxs,tsx} "],
+  theme: {
+    extend: {
+      colour {
+  "weather-primary": "00668A",
+  "weather-secondary": "004E71",
+
+      }
+
+      fontFamily: {
+        Roboto: ["Roboto, sans-serif"],
+
+      }
+    container: {
+      padding: "2rem",
+      center:true,
+
+    },
+   screens: {
+    sm: "640px",
+    md: "769px",
+   },
+
+
+    },
+
+  
+  plugins: [],
+};
+
